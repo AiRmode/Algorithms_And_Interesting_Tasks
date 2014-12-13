d774d0aba34983c1773d0c05b9eab3db5ec253b6
AiRmode <NeoNode-N1@yandex.ru>
2014-12-13 17:33:02 +0200

    It works, need some minor changes

--- src/rabbitAndTortoise/Test.java
+++ src/rabbitAndTortoise/Test.java
@@ -5,79 +5,105 @@ package rabbitAndTortoise;
  */
 public class Test {
 
+    private static final int COLLECTION_SIZE = 85;
+    private static final int COLLECTION_START_INDEX = 0;
+    private static final int BROKEN_ENTRY_NEXT_INDEX = 70;
+    private static final int BROKEN_ENTRY_PREV_INDEX = 6;
+    private static final int BROKEN_ENTRY_INDEX = 75;
     private static DataCollection.Entry brokenEntry;
 
     public static void main(String[] args) {
-        //few tests for verification, if collection give expected result or not.
-        brokenCaseTest();//test for broken scenario
-        normalCaseTest();
+        validateIndexes();
 
-        startSearchLoopedRecords();
+        //few tests for verification, if collection give expected result or not.
+        normalTestCollection();
+        brokenCaseTest();//test for broken scenario
+
+        detectLoop();
     }
 
-    private static void startSearchLoopedRecords() {
+    private static void validateIndexes() {
+        /**
+         private static final int COLLECTION_SIZE =85;
+         private static final int COLLECTION_START_INDEX = 0;
+         private static final int BROKEN_ENTRY_NEXT_INDEX = 81;
+         private static final int BROKEN_ENTRY_PREV_INDEX = 6;
+         private static final int BROKEN_ENTRY_INDEX = 80
+         */
+
+        if (COLLECTION_SIZE < COLLECTION_START_INDEX)
+            throw new ArrayIndexOutOfBoundsException();
+        if (COLLECTION_SIZE < BROKEN_ENTRY_NEXT_INDEX)
+            throw new ArrayIndexOutOfBoundsException();
+        if (BROKEN_ENTRY_INDEX < BROKEN_ENTRY_NEXT_INDEX)
+            throw new ArrayIndexOutOfBoundsException();
+
+    }
+
+    private static void detectLoop() {
         System.out.println("startSearchLoopedRecords..:");
         DataCollection.Entry rabbit = brokenEntry;
         DataCollection.Entry tortoise = brokenEntry;
-        for (int i = 0; i < 150; i++) {
+        int lap = 0;
+        while (true) {
+            lap++;
             tortoise = tortoise.getNextEntry().getNextEntry();
             rabbit = rabbit.getNextEntry();
             if (tortoise.equals(rabbit)) {
                 System.out.println("Loop was founded!");
                 System.out.println("Tortoise: " + tortoise);
                 System.out.println("Rabbit: " + rabbit);
-                System.out.println("Lap: " + i);
+                System.out.println("Lap: " + lap);
                 break;
             }
         }
     }
 
-    private static void normalCaseTest() {
+    private static void normalTestCollection() {
         DataCollection<String> dataCollection = new DataCollection<>();
-        for (int i = 0; i < 10; i++) {
-            dataCollection.add("" + i);
-        }
-
-        DataCollection<DataWrapper> dataCollection2 = new DataCollection<>();
         DataCollection.Entry n;
         DataCollection.Entry nFirst = null;
         DataCollection.Entry nLast = null;
-        DataCollection.Entry nRandom = null;
-        for (int i = 0; i < 10; i++) {
-            n = dataCollection2.add(new DataWrapper("" + i));
-            if (i == 0) {
+        DataCollection.Entry nMiddle = null;
+        for (int i = COLLECTION_START_INDEX; i < COLLECTION_SIZE; i++) {
+            n = dataCollection.add("" + i);
+            if (i == COLLECTION_START_INDEX) {
                 nFirst = n;
-            } else if (i == 9) {
+            } else if (i == COLLECTION_SIZE - 1) {
                 nLast = n;
-            } else if (i == 5) {
-                nRandom = n;
+            } else if (i == COLLECTION_SIZE / 2) {
+                nMiddle = n;
             }
         }
 
         System.out.println("Iterate forward");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(nFirst.getEntry());
+        for (int i = COLLECTION_START_INDEX; i < COLLECTION_SIZE; i++) {
+//            System.out.println(nFirst.getEntry());
             nFirst = nFirst.getNextEntry();
         }
 
         System.out.println("Iterate backward");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(nLast.getEntry());
+        for (int i = COLLECTION_START_INDEX; i < COLLECTION_SIZE; i++) {
+//            System.out.println(nLast.getEntry());
             nLast = nLast.getPrevEntry();
         }
 
-        System.out.println("Iterate from random element");
-        for (int i = 0; i < 6; i++) {
-            System.out.println(nRandom.getEntry());
-            if (nRandom.getPrevEntry() != null)
-                nRandom = nRandom.getPrevEntry();
+        System.out.println("Iterate from middle element");
+        System.out.println("Case: 1: <<<");
+        for (int i = COLLECTION_START_INDEX; i < COLLECTION_SIZE / 2 + 1; i++) {
+//            System.out.println(nMiddle.getEntry());
+            if (nMiddle.getPrevEntry() != null) {
+                nMiddle = nMiddle.getPrevEntry();
+            }
         }
-        for (int i = 0; i < 6; i++) {
-            System.out.println(nRandom.getEntry());
-            nRandom = nRandom.getNextEntry();
+        System.out.println("\nCase 2 ->>>");
+        for (int i = COLLECTION_START_INDEX; i < COLLECTION_SIZE / 2 + 1; i++) {
+//            System.out.println(nMiddle.getEntry());
+            if (nMiddle.getNextEntry() != null)
+                nMiddle = nMiddle.getNextEntry();
         }
 
-        System.out.println("-------------\n" + dataCollection);
+        System.out.println("------End of normal test------\n");
     }
 
     private static void brokenCaseTest() {
@@ -85,21 +111,22 @@ public class Test {
         DataCollection.Entry targetPrev = null;
         DataCollection.Entry targetNext = null;
         DataCollection.Entry targetToBreak = null;
-        for (int i = 1; i < 100; i++) {
-            if (i == 77) {
+        for (int i = COLLECTION_START_INDEX; i < COLLECTION_SIZE; i++) {
+            if (i == BROKEN_ENTRY_NEXT_INDEX) {
                 targetNext = dataCollection.add("" + i);
-            } else if (i == 6) {
+            } else if (i == BROKEN_ENTRY_PREV_INDEX) {
                 targetPrev = dataCollection.add("" + i);
-            } else if (i == 80) {
+            } else if (i == BROKEN_ENTRY_INDEX) {
                 targetToBreak = dataCollection.add("" + i);
             } else {
                 dataCollection.add("" + i);
             }
         }
+
         DataCollection.Entry brokenEntry = dataCollection.addBrokenEntry(targetToBreak, targetPrev, targetNext);
         setBrokenEntry(brokenEntry);
 
-        System.out.println(brokenEntry);
+        System.out.println("Hint: broken entry is: " + brokenEntry);
     }
 
     public static void setBrokenEntry(DataCollection.Entry brokenEntry) {
